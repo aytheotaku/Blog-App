038effee3db16f0fc32beec9a7adb39747269430
aytheotaku <ayakinbo@gmail.com>
2022-12-12 22:54:09 +0100

    set ejs to be the view engine

--- app.js
+++ app.js
@@ -1,13 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
-
 const app = express();
 
-
-
-
-
-
+app.set('view-engine', 'ejs')
 
 app.listen(3000, ()=> console.log(`listening on port 3000`))
