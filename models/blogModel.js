aea1c88a9710d553e5b5260febb630616d949341
aytheotaku <ayakinbo@gmail.com>
2022-12-13 01:47:12 +0100

    created a model from the schema

--- models/blogModel.js
+++ models/blogModel.js
@@ -2,10 +2,16 @@ const mongoose = require('mongoose');
 
 const { Schema } = mongoose;
 
-
+//defining database schema with timestamps
 const blogSchema = new Schema({
     blogTitle: {type: String, required: true},
     blogSnippet: {type: String, required: true},
     blogBody: {type: String, required: true},
 
-})
+}, { timestamps: true } )
+
+// creating a model with the defined schema above.
+const blogModel = mongoose.model('blog', blogSchema)
+
+
+module.exports = blogModel;
