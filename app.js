d2d4ba5ecb53185f02788e62398a8e7de1aa60a6
aytheotaku <ayakinbo@gmail.com>
2022-12-13 01:17:58 +0100

    added views and partials

--- app.js
+++ app.js
@@ -3,6 +3,18 @@ const mongoose = require('mongoose');
 
 const app = express();
 
+// setting ejs as view engine
 app.set('view-engine', 'ejs')
+
+//setting routes
+app.get('/', (req,res) => {
+    const blog = []
+    res.render('index.ejs', {pageTitle: 'Home-Page', blogs:blog})
+})
+
+app.get('/blogs/add-blog', (req, res) => {
+    res.render('add-blog.ejs', {pageTitle: "New Blog"})
+})
+
 
 app.listen(3000, ()=> console.log(`listening on port 3000`))
