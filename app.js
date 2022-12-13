b758415fd3a2dc6f1b0d24d543e01555c82f851d
aytheotaku <ayakinbo@gmail.com>
2022-12-13 21:23:41 +0100

    redirected home route to blogs, created an addblog route, required a bodyParser module

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
+const { render } = require('ejs');
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const blogModel = require('./models/blogModel.js');
 
-//
+//database connection string
 const databaseConnectionString = 'mongodb+srv://aytheotaku:stardustcrusaders123@nodecluster.xpuvq9k.mongodb.net/BlogsDatabase?retryWrites=true&w=majority'
 
 mongoose.set('strictQuery', true);
@@ -18,14 +21,63 @@ const app = express();
 // setting ejs as view engine
 app.set('view-engine', 'ejs')
 
+//using express json middleware
+
+app.use(express.json());
+
+//using body parser middleware
+app.use(bodyParser.urlencoded({extended: false}))
+
+
 //setting routes
 app.get('/', (req,res) => {
-    const blog = []
-    res.render('index.ejs', {pageTitle: 'Home-Page', blogs:blog})
+    
+  res.status(301).redirect('/blogs')
+})
+
+app.get('/blogs', (req, res) => {
+    blogModel.find().sort({createdAt: -1})
+        .then(result => {
+            res.render('index.ejs', {pageTitle:'Home-Page', blogs: result})
+        })
+        .catch(err => console.log(err));
 })
 
 app.get('/blogs/add-blog', (req, res) => {
-    res.render('add-blog.ejs', {pageTitle: "New Blog"})
+
+    res.render('add-blog.ejs', { pageTitle: "New Blog"})
 })
 
+app.post('/blogs/add-blog', (req, res) => {
+
+    const { blogTitle, blogSnippet, blogBody } = req.body;
+
+    const blog = new blogModel({
+        blogTitle,
+        blogSnippet,
+        blogBody
+    })
+
+    blog.save()
+        .then(() => {
+            console.log('data succesfully added')
+            res.redirect('/blogs')
+        })
+        .catch(err => console.log(err))
+})
+
+
+app.get('/blogs/:id' , (req, res) => {
+   blogModel.findById(req.params.id)
+    .then(result => {
+        res.render('about-blog.ejs', {pageTitle: 'Blog', blog: result})
+    })
+    .catch(err => console.log(err)) 
+})
+
+
+
+app.use((req, res) => {
+    res.status(404).render('404.ejs', {pageTitle : "404 page"})
+})
 
