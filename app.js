5fd373897dc9e0a678ec4f6eb2405cf81ca3e37f
aytheotaku <ayakinbo@gmail.com>
2022-12-15 03:14:30 +0100

    used routehandler middleware for blogs path

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const blogModel = require('./models/blogModel.js');
+const blogRouter = require('./routerHandler/blogRouter.js')
 require('dotenv').config()
 
 //database connection string
@@ -32,66 +33,12 @@ app.use(express.static('public'));
 //using body parser middleware
 app.use(bodyParser.urlencoded({extended: false}))
 
-
-//setting routes
-app.get('/', (req,res) => {
-    
-  res.status(301).redirect('/blogs')
+app.get('/', (req, res) => {
+    res.redirect('/blogs')
 })
 
-app.get('/blogs', (req, res) => {
-    blogModel.find().sort({createdAt: -1})
-        .then(result => {
-            res.render('index.ejs', {pageTitle:'Home-Page', blogs: result})
-        })
-        .catch(err => console.log(err));
-})
-
-app.get('/blogs/add-blog', (req, res) => {
-
-    res.render('add-blog.ejs', { pageTitle: "New Blog"})
-})
-
-app.post('/blogs/add-blog', (req, res) => {
-
-    const { blogTitle, blogSnippet, blogBody } = req.body;
-
-    const blog = new blogModel({
-        blogTitle,
-        blogSnippet,
-        blogBody
-    })
-
-    blog.save()
-        .then(() => {
-            res.redirect('/blogs')
-        })
-        .catch(err => console.log(err))
-})
-
-
-app.get('/blogs/:id' , (req, res) => {
-   blogModel.findById(req.params.id)
-    .then(result => {
-        res.render('about-blog.ejs', {pageTitle: 'Blog', blog: result})
-    })
-    .catch(err => {
-        res.status(404).render('404.ejs', {pageTitle : "404 page"})
-    }) 
-})
-
-app.delete('/blogs/:id' , (req, res) => {
-    blogModel.findByIdAndRemove(req.params.id)
-        .then(result =>{
-            console.log('data removed')
-            console.log(result)
-            res.json({ urlRedirect: '/blogs' })
-        })
-        .catch(err => console.log(err))
-
-})
- 
-
+//setting blog Routes
+app.use('/blogs', blogRouter);
 
 
 app.use((req, res) => {
